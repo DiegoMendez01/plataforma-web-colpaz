013eab955f8baba21297483a2df17b8b7bb1017f
fernando00q <fernandoibarra1010@gmail.com>
2023-12-09 20:56:37 -0500

    Se agrego código a las carpetas MainHead, MainHeader, MainJs y MainNav para mejorar la estructura y vistas del proyecto.

---
+++ views/mainNav/nav.php
@@ -0,0 +1,121 @@
+<?php 
+if($_SESSION['role_id'] == 1){
+?>
+    <nav class="side-menu">
+        <ul class="side-menu-list">
+        	<li class="blue-dirty">
+        		<a href="..\home\">
+    				<span class="glyphicon glyphicon-th"></span>
+    				<span class="lbl">Inicio</span>
+    			</a>
+    		</li>
+    		<li class="blue-dirty">
+    			<a href="../gestionUsuarios/">
+    				<span class="glyphicon glyphicon-th"></span>
+    				<span class="lbl">Gestion de Usuarios</span>
+    			</a>
+    		</li>
+    		<li class="blue-dirty">
+    			<a href="#">
+    				<span class="glyphicon glyphicon-th"></span>
+    				<span class="lbl">Gestion Cursos</span>
+    			</a>
+    		</li>
+    		<li class="blue-dirty">
+    			<a href="#">
+    				<span class="glyphicon glyphicon-th"></span>
+    				<span class="lbl">Gestion Materias</span>
+    			</a>
+    		</li>
+    	</ul>
+    </nav>
+<?php 
+}else if($_SESSION['role_id'] == 2){
+?>
+	<nav class="side-menu">
+        <ul class="side-menu-list">
+        	<li class="blue-dirty">
+        		<a href="..\home\">
+    				<span class="glyphicon glyphicon-th"></span>
+    				<span class="lbl">Inicio</span>
+    			</a>
+    		</li>
+    		<li class="blue-dirty">
+    			<a href="#">
+    				<span class="glyphicon glyphicon-th"></span>
+    				<span class="lbl">Detalle Cursos</span>
+    			</a>
+    		</li>
+    		<li class="blue-dirty">
+    			<a href="#">
+    				<span class="glyphicon glyphicon-th"></span>
+    				<span class="lbl">Detalle Materias</span>
+    			</a>
+    		</li>
+    		<li class="blue-dirty">
+    			<a href="#">
+    				<span class="glyphicon glyphicon-th"></span>
+    				<span class="lbl">Detalle estudiantes</span>
+    			</a>
+    		</li>
+    	</ul>
+    </nav>
+<?php 
+}else if($_SESSION['role_id'] == 3){
+?>
+	<nav class="side-menu">
+        <ul class="side-menu-list">
+        	<li class="blue-dirty">
+        		<a href="..\home\">
+    				<span class="glyphicon glyphicon-th"></span>
+    				<span class="lbl">Inicio</span>
+    			</a>
+    		</li>
+    		<li class="blue-dirty">
+    			<a href="#">
+    				<span class="glyphicon glyphicon-th"></span>
+    				<span class="lbl">Mi Curso</span>
+    			</a>
+    		</li>
+    		<li class="blue-dirty">
+    			<a href="#">
+    				<span class="glyphicon glyphicon-th"></span>
+    				<span class="lbl">Mis Materias</span>
+    			</a>
+    		</li>
+    		<li class="blue-dirty">
+    			<a href="#">
+    				<span class="glyphicon glyphicon-th"></span>
+    				<span class="lbl">Mis Amigos</span>
+    			</a>
+    		</li>
+    	</ul>
+    </nav>
+<?php
+}else{
+?>
+	<nav class="side-menu">
+        <ul class="side-menu-list">
+        	<li class="blue-dirty">
+        		<a href="..\home\">
+    				<span class="glyphicon glyphicon-th"></span>
+    				<span class="lbl">Inicio</span>
+    			</a>
+    		</li>
+    		<li class="blue-dirty">
+    			<a href="#">
+    				<span class="glyphicon glyphicon-th"></span>
+    				<span class="lbl">Contacto</span>
+    			</a>
+    		</li>
+    		<li class="blue-dirty">
+    			<a href="#">
+    				<span class="glyphicon glyphicon-th"></span>
+    				<span class="lbl">Curso de prueba</span>
+    			</a>
+    		</li>
+    	</ul>
+    </nav>
+<?php 
+}
+?>
