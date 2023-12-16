59c6fd83d1cb156c6e44cd244bab918ecfff8a77
DiegoMendez01 <diegomendez01soft@gmail.com>
2023-12-15 20:59:04 -0500

    Agregar funciones de eliminar y actualizar al modelo de cursos

--- models/Courses.php
+++ models/Courses.php
@@ -5,26 +5,50 @@ class Courses extends Connect
     /*
      * Funcion para insertar/registrar cursos por medio de un formulario
      */
-    public function insertCourse($parent_id, $name, $description = null)
+    public function insertCourse($name, $description = null)
     {
         $conectar = parent::connection();
         parent::set_names();
         
-        // Concatenar y formatear las credenciales para generar el API key
-        $token = sprintf("%s-%s-%s-%s-%s", substr(md5($name), 0, 8), substr(md5($parent_id), 0, 4), substr(md5(uniqid()), 0, 4), substr(md5(uniqid()), 0, 4), substr(md5(uniqid()), 0, 8));
+        // Concatenar y formatear las credenciales para generar el token
+        $token = sprintf("%s-%s-%s-%s-%s", substr(md5($name), 0, 8), substr(md5($name), 0, 4), substr(md5(uniqid()), 0, 4), substr(md5(uniqid()), 0, 4), substr(md5(uniqid()), 0, 8));
         
         $sql = "
             INSERT INTO
                 courses (name, description, token, created)
             VALUES
-                (?, ?, ?, ?, now())
+                (?, ?, ?, now())
         ";
         
         $stmt = $conectar->prepare($sql);
         $stmt->bindValue(1, $name);
-        $stmt->bindValue(2, $parent_id);
-        $stmt->bindValue(3, $description);
-        $stmt->bindValue(4, $token);
+        $stmt->bindValue(2, $description);
+        $stmt->bindValue(3, $token);
+        $stmt->execute();
+        
+        return $result = $stmt->fetchAll();
+    }
+    /*
+     * Funcion para actualizar registros de cursos existentes por su ID
+     */
+    public function updateCourse($id, $name, $description = null)
+    {
+        $conectar = parent::connection();
+        parent::set_names();
+        
+        $sql = "
+            UPDATE
+                courses
+            SET
+                name = ?, description = ?
+            WHERE
+                id = ?
+        ";
+        
+        $stmt = $conectar->prepare($sql);
+        $stmt->bindValue(1, $name);
+        $stmt->bindValue(2, $description);
+        $stmt->bindValue(3, $id);
         $stmt->execute();
         
         return $result = $stmt->fetchAll();
@@ -47,6 +71,29 @@ class Courses extends Connect
         ";
         
         $stmt = $conectar->prepare($sql);
+        $stmt->execute();
+        
+        return $result = $stmt->fetchAll();
+    }
+    /*
+     * Funcion para eliminar totalmente registros de cursos existentes por su ID
+     */
+    public function deleteCourseById($id)
+    {
+        $conectar = parent::connection();
+        parent::set_names();
+        
+        $sql = "
+            UPDATE
+                courses
+            SET
+                is_active = 0
+            WHERE
+                id = ?
+        ";
+        
+        $stmt = $conectar->prepare($sql);
+        $stmt->bindValue(1, $id);
         $stmt->execute();
         
         return $result = $stmt->fetchAll();
