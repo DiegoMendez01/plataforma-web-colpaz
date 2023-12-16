754173acce1075ee8d768d27f43037480377fce8
DiegoMendez01 <diegomendez01soft@gmail.com>
2023-12-15 20:51:33 -0500

    Ajustes en el controlador de los grados academicos

--- controllers/gradeController.php
+++ controllers/gradeController.php
@@ -3,46 +3,56 @@
 require_once("../config/connection.php");
 require_once("../models/Grades.php");
 
-$gradesModel = new Grades();
+$grades = new Grades();
 
-switch($_GET['op']){
+switch($_GET['op'])
+{
     /*
-     * Insertar o actualizar el registro de un grado académico. Dependiendo si existe o no el grado,
-     * se tomará un flujo.
+     * Insertar o actualizar el registro de un grado academico. Dependiendo si existe o no el grado,
+     * se tomara un flujo.
      */
     case 'insertOrUpdate':
         if(empty($_POST['id'])){
-            $gradesModel->insertGrade($_POST['name']);
+            $grades->insertGrade($_POST['name']);
         } else {
-            $gradesModel->updateGrade($_POST['id'], $_POST['name']);
+            $grades->updateGrade($_POST['id'], $_POST['name']);
         }
         break;
     /*
-     * Es para listar/obtener los grados académicos que existen registrados en el sistema con una condición que este activo.
+     * Es para listar/obtener los grados academicos que existen registrados en el sistema con una condicion que este activo.
      * Ademas, de dibujar una tabla para mostrar los registros.
      */
     case 'listGrade':
-        $grades = $gradesModel->getGrades();
-
-        echo "<h2>Grados Académicos:</h2>";
-        foreach ($grades as $grade) {
-            echo "ID: " . $grade['id'] . ", Nombre: " . $grade['nombre'] . "<br>";
+        $datos = $grades->getGrades();
+        
+        foreach ($datos as $row) {
+            $sub_array      = [];
+            $sub_array[]    = $row['name'];
+            $sub_array[]    = $row['created'];
+            if($row['is_active'] == 1){
+                $sub_array[] = 'Activo';
+            }
+            
+            $sub_array[] = '<button type="button" onClick="editar('.$row["id"].')"; id="'.$row['id'].'" class="btn btn-inline btn-warning btn-sm ladda-button"><i class="fa fa-edit"></i></button>';
+            $sub_array[] = '<button type="button" onClick="eliminar('.$row["id"].')"; id="'.$row['id'].'" class="btn btn-inline btn-danger btn-sm ladda-button"><i class="fa fa-trash"></i></button>';
+            $sub_array[] = '<button type="button" onClick="ver('.$row["id"].')"; id="'.$row['id'].'" class="btn btn-inline btn-primary btn-sm ladda-button"><i class="fa fa-eye"></i></button>';
+            
+            $data[] = $sub_array;
         }
+        $results = [
+            "sEcho"                 => 1,
+            "iTotalRecords"         => count($data),
+            "iTotalDisplayRecords"  => count($data),
+            "aaData"                => $data
+        ];
+        echo json_encode($results);
         break;
     /*
-     * Actualizar registros de grados académicos.
-     */
-    case 'updateGrade':
-        if(isset($_POST['id'])){
-            $gradesModel->updateGrade($_POST['id'], $_POST['name']);
-        }
-        break;
-    /*
-     * Eliminar totalmente registros de grados académicos existentes (eliminado lógico).
+     * Eliminar totalmente registros de grados academicos existentes por su ID (eliminado logico).
      */
     case 'deleteGradeById':
         if(isset($_POST['id'])){
-            $gradesModel->deleteGradeById($_POST['id']);
+            $grades->deleteGradeById($_POST['id']);
         }
         break;
 }
