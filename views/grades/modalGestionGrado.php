844b8fc7d2fdb0450568e064c3b45b69da591fb6
villamar <cristianvillamar097@gmail.com>
2023-12-15 21:12:46 -0500

    modalgestiongrado faltante por cargar

---
+++ views/grades/modalGestionGrado.php
@@ -0,0 +1,27 @@
+<div id="modalGestionGrado" class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="model-close" data-dismiss="modal" aria-label="Close">
+                    <i class="font-icon-close-2"></i>
+                </button>
+                <h4 class="modal-title" id="mdltitulo"></h4>
+            </div>
+            <form method="post" id="grade_form">
+                <div class="modal-body">
+                    <input type="hidden" id="id" name="id" />
+                    <div class="form-group row">
+                        <label for="name" class="col-sm-2 form-control-label">Nombre <b>*</b></label>
+                        <div class="col-sm-10">
+                            <p class="form-control-static"><input type="text" class="form-control" id="name" name="name" placeholder="Ingrese el nombre del grado"></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-rounded btn-default" data-dismiss="modal">Cerrar</button>
+                    <button type="submit" name="action" id="btnGuardar" class="btn btn-rounded btn-primary">Guardar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
