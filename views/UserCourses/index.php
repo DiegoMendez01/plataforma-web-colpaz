59f0875ab1636eee06d53a782f9df855f59aee4d
DiegoMendez01 <diegomendez01soft@gmail.com>
2024-01-05 21:45:20 -0500

    Ajustes minimos al index de usercourse

--- views/UserCourses/index.php
+++ views/UserCourses/index.php
@@ -11,7 +11,7 @@ if (isset($_SESSION['id'])) {
 
 <head>
     <?php require_once("../MainHead/head.php"); ?>
-    <title>Aula Virtual::Gestion de Grados</title>
+    <title>Aula Virtual::Cursos Usuarios</title>
 </head>
 
 <body class="with-side-menu">
@@ -27,31 +27,40 @@ if (isset($_SESSION['id'])) {
                 <div class="tbl">
                     <div class="tbl-row">
                         <div class="tbl-cell">
-                            <h3>Gestion de Grados</h3>
+                            <h3>Cursos Usuarios</h3>
                             <ol class="breadcrumb breadcrumb-simple">
                                 <li><a href="../Home/">Inicio</a></li>
-                                <li class="active">Gestion de Grados</li>
+                                <li class="active">Cursos Usuarios</li>
                             </ol>
                         </div>
                     </div>
                 </div>
             </header>
-
+            
             <div class="box-typical box-typical-padding">
                 <button type="button" id="btnnuevo" class="btn btn-inline btn-primary">Nuevo Registro</button>
-                <table id="userCourse_data" class="table table-bordered table-striped table-vcenter js-dataTable-full">
+                <table id="usercourse_data" class="table table-bordered table-striped table-vcenter js-dataTable-full">
                     <thead>
                         <tr>
-                            <th style="width: 15%;">Nombre</th>
-                            <th style="width: 30%;">Creado</th>
+                            <th style="width: 15%;">Curso</th>
+                            <th style="width: 30%;">Usuario</th>
                             <th class="d-none d-sm-table-cell" style="width: 25%;">Estado</th>
                             <th class="text-center" style="width: 5%"></th>
                             <th class="text-center" style="width: 5%"></th>
                             <th class="text-center" style="width: 5%"></th>
                         </tr>
                     </thead>
-
-
+                </table>
+            </div>
+        </div>
+    </div>
+    
+    <?php require_once("modalGestionUserCourse.php"); ?>
+    
+    <?php require_once ("../MainJs/js.php"); ?>
+    
+    <script src="usercourses.js" type="text/javascript"></script>
+</body>
 </html>
 
 <?php
