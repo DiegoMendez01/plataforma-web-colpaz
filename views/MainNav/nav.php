667a6a171cac598f25315cb6ae9b379a8fd82219
DiegoMendez01 <diegomendez01soft@gmail.com>
2024-01-05 21:37:31 -0500

    Ajustes minimos al modal y el nav del sistema

--- views/MainNav/nav.php
+++ views/MainNav/nav.php
@@ -1,14 +1,14 @@
-<?php 
-if($_SESSION['role_id'] == 1){
-?>
-    <nav class="side-menu">
-        <ul class="side-menu-list">
-        	<li class="blue-dirty">
-        		<a href="..\Home\">
-    				<span class="glyphicon glyphicon-th"></span>
-    				<span class="lbl">Inicio</span>
-    			</a>
-    		</li>
+<nav class="side-menu">
+    <ul class="side-menu-list">
+    	<li class="blue-dirty">
+    		<a href="..\Home\">
+				<span class="glyphicon glyphicon-th"></span>
+				<span class="lbl">Inicio</span>
+			</a>
+		</li>
+		<?php 
+		if($_SESSION['role_id'] == 1 OR $_SESSION['role_id'] == 2){
+        ?>
     		<li class="blue-dirty">
     			<a href="..\Users\">
     				<span class="glyphicon glyphicon-th"></span>
@@ -27,95 +27,23 @@ if($_SESSION['role_id'] == 1){
     				<span class="lbl">Gestion Grados</span>
     			</a>
     		</li>
-    	</ul>
-    </nav>
-<?php 
-}else if($_SESSION['role_id'] == 2){
-?>
-	<nav class="side-menu">
-        <ul class="side-menu-list">
+			<li class="blue-dirty">
+    			<a href="..\UserCourses\">
+    				<span class="glyphicon glyphicon-th"></span>
+    				<span class="lbl">Cursos Usuarios</span>
+    			</a>
+    		</li>
+		<?php 
+		}elseif($_SESSION['role_id'] == 3){
+        ?>
         	<li class="blue-dirty">
-        		<a href="..\home\">
+    			<a href="..\UserCourses\">
     				<span class="glyphicon glyphicon-th"></span>
-    				<span class="lbl">Inicio</span>
+    				<span class="lbl">Cursos Usuarios</span>
     			</a>
     		</li>
-    		<li class="blue-dirty">
-    			<a href="#">
-    				<span class="glyphicon glyphicon-th"></span>
-    				<span class="lbl">Detalle Cursos</span>
-    			</a>
-    		</li>
-    		<li class="blue-dirty">
-    			<a href="#">
-    				<span class="glyphicon glyphicon-th"></span>
-    				<span class="lbl">Detalle Materias</span>
-    			</a>
-    		</li>
-    		<li class="blue-dirty">
-    			<a href="#">
-    				<span class="glyphicon glyphicon-th"></span>
-    				<span class="lbl">Detalle estudiantes</span>
-    			</a>
-    		</li>
-    	</ul>
-    </nav>
-<?php 
-}else if($_SESSION['role_id'] == 3){
-?>
-	<nav class="side-menu">
-        <ul class="side-menu-list">
-        	<li class="blue-dirty">
-        		<a href="..\home\">
-    				<span class="glyphicon glyphicon-th"></span>
-    				<span class="lbl">Inicio</span>
-    			</a>
-    		</li>
-    		<li class="blue-dirty">
-    			<a href="#">
-    				<span class="glyphicon glyphicon-th"></span>
-    				<span class="lbl">Mi Curso</span>
-    			</a>
-    		</li>
-    		<li class="blue-dirty">
-    			<a href="#">
-    				<span class="glyphicon glyphicon-th"></span>
-    				<span class="lbl">Mis Materias</span>
-    			</a>
-    		</li>
-    		<li class="blue-dirty">
-    			<a href="#">
-    				<span class="glyphicon glyphicon-th"></span>
-    				<span class="lbl">Mis Amigos</span>
-    			</a>
-    		</li>
-    	</ul>
-    </nav>
-<?php
-}else{
-?>
-	<nav class="side-menu">
-        <ul class="side-menu-list">
-        	<li class="blue-dirty">
-        		<a href="..\home\">
-    				<span class="glyphicon glyphicon-th"></span>
-    				<span class="lbl">Inicio</span>
-    			</a>
-    		</li>
-    		<li class="blue-dirty">
-    			<a href="#">
-    				<span class="glyphicon glyphicon-th"></span>
-    				<span class="lbl">Contacto</span>
-    			</a>
-    		</li>
-    		<li class="blue-dirty">
-    			<a href="#">
-    				<span class="glyphicon glyphicon-th"></span>
-    				<span class="lbl">Curso de prueba</span>
-    			</a>
-    		</li>
-    	</ul>
-    </nav>
-<?php 
-}
-?>
+        <?php 
+        }
+        ?>
+	</ul>
+</nav>
