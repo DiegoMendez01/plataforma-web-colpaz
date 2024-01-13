433a327c246226f692927fd989f798c9c3acf171
fernando00q\ferna <fernandoibarra1010@gmail.com>
2024-01-12 19:56:28 -0500

    Controlador para el CRUD de la entidad Roles

---
+++ controllers/RolesController.php
@@ -0,0 +1,74 @@
+<?php
+// Importa la clase del modelo
+require_once("../config/connection.php");
+require_once("../models/Roles.php");
+
+$roles = new Roles();
+
+switch($_GET['op'])
+{
+    /*
+     * Insertar o actualizar el registro de un grado academico. Dependiendo si existe o no el grado,
+     * se tomara un flujo.
+     */
+    case 'insertOrUpdate':
+        if(empty($_POST['id'])){
+            $roles->insertRoles($_POST['name']);
+        } else {
+            $roles->updateRoles($_POST['id'], $_POST['name']);
+        }
+        break;
+    /*
+     * Es para listar/obtener los grados academicos que existen registrados en el sistema con una condicion que este activo.
+     * Ademas, de dibujar una tabla para mostrar los registros.
+     */
+    case 'listRoles':
+        $datos = $roles->getRoles();
+        
+        foreach ($datos as $row) {
+            $sub_array      = [];
+            $sub_array[]    = $row['name'];
+            $sub_array[]    = $row['created'];
+            if($row['is_active'] == 1){
+                $sub_array[] = 'Activo';
+            }
+            
+            $sub_array[] = '<button type="button" onClick="editar('.$row["id"].')"; id="'.$row['id'].'" class="btn btn-inline btn-warning btn-sm ladda-button"><i class="fa fa-edit"></i></button>';
+            $sub_array[] = '<button type="button" onClick="eliminar('.$row["id"].')"; id="'.$row['id'].'" class="btn btn-inline btn-danger btn-sm ladda-button"><i class="fa fa-trash"></i></button>';
+            $sub_array[] = '<button type="button" onClick="ver('.$row["id"].')"; id="'.$row['id'].'" class="btn btn-inline btn-primary btn-sm ladda-button"><i class="fa fa-eye"></i></button>';
+            
+            $data[] = $sub_array;
+        }
+        $results = [
+            "sEcho"                 => 1,
+            "iTotalRecords"         => count($data),
+            "iTotalDisplayRecords"  => count($data),
+            "aaData"                => $data
+        ];
+        echo json_encode($results);
+        break;
+    /*
+     * Eliminar totalmente registros de grados academicos existentes por su ID (eliminado logico).
+     */
+    case 'deleteRolesById':
+        if(isset($_POST['id'])){
+            $roles->deleteRolesById($_POST['id']);
+        }
+        break;
+    /*
+     * Es para listar/obtener los usuarios que existen registrados en el sistema.
+     * Pero debe mostrar el usuario por medio de su identificador unico
+     */
+    case 'listRolesById':
+        $datos = $roles->getRolesById($_POST['id']);
+        
+        if(is_array($datos) == true AND count($datos)){
+            foreach($datos as $row){
+                $output["id"]                       = $row['id'];
+                $output["name"]                     = $row['name'];
+            }
+            echo json_encode($output);
+        }
+        break;
+}
+?>
