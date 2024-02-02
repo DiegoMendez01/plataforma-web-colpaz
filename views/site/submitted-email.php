bf47b9733fa4690a2d58046051aa76480e4e8c47
DiegoMendez01 <diegomendez01soft@gmail.com>
2024-02-01 20:56:28 -0500

    Ajuste de la vista de reenvio de correo electronico

--- views/site/submitted-email.php
+++ views/site/submitted-email.php
@@ -1,24 +1,54 @@
 <?php
-
 require_once("../../config/connection.php");
-
 ?>
 <!DOCTYPE html>
-<html>
-<head lang="es">
-	<?php
+<html lang="es">
+<head>
+    <?php
     require_once ("../mainHead/head.php");
     ?>
-    <title>Aula Virtual:Reenviar Correo Electronico</title>
+    <title>Aula Virtual: Reenviar Correo Electrónico</title>
+    <!-- Incluye Bootstrap y FontAwesome -->
+    <link rel="stylesheet" href="../../public/css/lib/font-awesome/font-awesome.min.css">
+    <link rel="stylesheet" href="../../public/css/lib/bootstrap/bootstrap.min.css">
 </head>
-<body class="with-side-menu">
-    <!-- Contenido  -->
-	<div class="page-content">
+<body>
+    <div class="page-content" style="margin-top: 0; padding-top: 20px;">
+        <div class="container-fluid">
+            <header class="section-header">
+                <div class="tbl">
+                    <div class="tbl-row">
+                        <div class="tbl-cell">
+                            <ol class="breadcrumb breadcrumb-simple">
+                                <li><a href="../site/">Inicio</a></li>
+                                <li class="active">Reenvio de Correo</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <div class="box-typical box-typical-padding">
+                <p>
+                    Módulo que permite al usuario el reenvio de un correo electronico para confirmar su cuenta.
+                </p>
+                <div class="col-md-12 text-center">
+                    <img class="hidden-md-down" src="../../public/img/LogoCOLPAZ.png" alt="Logo" style="height: 20vh;">
+                </div>
+                <h2>Formulario de Reenvío de Correo Electrónico</h2>
+                <form method="post" id="submitted_email">
+                    <div class="form-group">
+                        <label for="email">Correo Electronico <b>*</b></label>
+                        <input type="email" id="email" name="email" class="form-control" placeholder="Ingrese la dirección de correo electronico">
+                    </div>
+                    <div class="text-center">
+                    	<button type="submit" class="btn btn-primary">Enviar Correo</button>
+                    </div>
+                </form>
+            </div>
+		</div>
 	</div>
-    
     <?php
     require_once ("../mainJs/js.php");
     ?>
-    <script src="submittedEmail.js" type="text/javascript"></script>
 </body>
 </html>
