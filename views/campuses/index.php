853c948c4a0f3072f83af776ce0e92158d49726f
NellyMartinezgomez <152571813+NellyMartinezgomez@users.noreply.github.com>
2024-02-06 21:27:15 -0500

    Views campuses /modal campuses

---
+++ views/campuses/index.php
@@ -0,0 +1,80 @@
+<?php
+
+require_once("../../config/connection.php");
+
+if(isset($_SESSION['id'])){
+?>
+<!DOCTYPE html>
+<html>
+<head lang="es">
+	<?php
+    require_once ("../mainHead/head.php");
+    ?>
+    <title>Aula Virtual:Gestion Sedes</title>
+</head>
+<body class="with-side-menu">
+	
+	<?php
+    require_once ("../mainHeader/header.php");
+    ?>
+	<!--.site-header-->
+
+	<div class="mobile-menu-left-overlay"></div>
+	
+	<?php
+    require_once ("../mainNav/nav.php");
+    ?>
+    
+    <!-- Contenido  -->
+	<div class="page-content">
+		<div class="container-fluid">
+			<header class="section-header">
+				<div class="tbl">
+					<div class="tbl-row">
+						<div class="tbl-cell">
+							<h3>Gestion Sedes</h3>
+							<ol class="breadcrumb breadcrumb-simple">
+								<li><a href="../home/">Inicio</a></li>
+								<li class="active">Gestion Sedes</li>
+							</ol>
+						</div>
+					</div>
+				</div>
+			</header>
+			
+			<div class="box-typical box-typical-padding">
+				<button type="button" id="btnnuevo" class="btn btn-inline btn-primary">Nuevo Registro</button>
+				<table id="degree_data" class="table table-bordered table-striped table-vcenter js-dataTable-full">
+					<thead>
+						<tr>
+							<th style="width: 30%;">Nombre</th> 
+							<th style="width: 30%;">Creado</th> 
+							<th class="d-none d-sm-table-cell" style="width: 25%;">Estado</th>
+							<th class="text-center" style="width: 5%"></th>
+							<th class="text-center" style="width: 5%"></th>
+							<th class="text-center" style="width: 5%"></th>
+						</tr>
+					</thead>
+					<tbody>
+					</tbody>
+				 </table>
+			</div>
+		</div>
+	</div>
+    
+    <?php
+    require_once("modalGestionCampuses.php");
+    ?>
+    
+    <?php
+    require_once ("../mainJs/js.php");
+    ?>
+    <script src="campuses.js" type="text/javascript"></script>
+</body>
+</html>
+<?php 
+}else{
+    header("Location:" . Connect::route() . "views/site/");
+    exit;
+}
+?>
